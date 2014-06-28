b3b244aed97ac1e427f017f283c6a6320681d87d
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-06-28 10:46:30 -0700

    Add some basic unit testing.

---
+++ passwand/Tests.py
@@ -0,0 +1,21 @@
+import Encryption, sys, unittest
+
+class Encrypt(unittest.TestCase):
+    def test_not_id(self):
+        '''If some text encrypts to itself, we've likely done something wrong.'''
+        m = 'master password'
+        t = 'hello world'
+        c, _, _ = Encryption.encrypt(m, t)
+        self.assertNotEqual(t, c)
+    def test_dual(self):
+        '''We can successfully encrypt and decrypt some text.'''
+        m = 'master password'
+        p = 'hello world'
+        c, salt, iv = Encryption.encrypt(m, p)
+        d = Encryption.decrypt(m, c, salt, iv)
+        self.assertEqual(p, d)
+
+def main():
+    unittest.main()
+if __name__ == '__main__':
+    sys.exit(main())
