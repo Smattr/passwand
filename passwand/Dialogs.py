e57e5efa4441def1f5f8838875bbd0cf9072fde8
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-06-29 22:25:54 -0700

    Add function for asking for a password.

--- passwand/Dialogs.py
+++ passwand/Dialogs.py
@@ -13,7 +13,14 @@ def show_message(message, title):
         init()
     tkMessageBox.showinfo(message, title)
 
-def ask_question(message, text, title):
+def askstring(message, text, title, **kwargs):
     if not initialised:
         init()
-    return tkSimpleDialog.askstring(title, message, initialvalue=text)
+    return tkSimpleDialog.askstring(title, message, initialvalue=text, **kwargs)
+
+def ask_question(message, text, title):
+    return askstring(message, text, title)
+
+def ask_password(message, text, title):
+    # Passing 'show' here is undocumented, but seems to work.
+    return askstring(message, text, title, show='*')
