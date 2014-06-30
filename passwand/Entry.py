d2c1be257e40fac9a6900ccf25a90fdc57940c7c
Matthew Fernandez <matthew.fernandez@gmail.com>
2014-06-29 17:33:02 -0700

    Add entry format.

---
+++ passwand/Entry.py
@@ -0,0 +1,38 @@
+import json
+from Encoding import decode, encode
+
+class Entry(object):
+    def __init__(self, properties):
+        def get(p):
+            v = properties.get(p)
+            if v is not None:
+                v = decode(v)
+            return v
+        self.service = get('service')
+        self.field = get('field')
+        self.value = get('value')
+
+    def to_dict(self):
+        d = {}
+        def put(f):
+            v = getattr(self, f)
+            if v is not None:
+                d[f] = encode(v)
+        put('service')
+        put('field')
+        put('value')
+        return d
+
+def read_entries(path):
+    with open(path, 'r') as f:
+        entries = json.load(f)
+    if not isinstance(entries, list):
+        raise Exception('unexpected data encountered')
+    for e in entries:
+        if not isinstance(e, dict):
+            raise Exception('unexpected data encountered')
+        yield Entry(e)
+
+def write_entries(path, entries):
+    with open(path, 'w') as f:
+        json.dump(map(Entry.to_dict, entries), f)
--- passwand/__init__.py
+++ passwand/__init__.py
@@ -1,3 +1,4 @@
 from Dialogs import show_message, ask_question
 from Encoding import decode, encode
 from Encryption import decrypt, encrypt
+from Entry import Entry, read_entries, write_entries
