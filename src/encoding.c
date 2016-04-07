19dcfa9e80438b527017fbc4a224629017e288fd
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-07 15:38:18 +1000

    encoding: Rename some variables to make the process clearer.

--- src/encoding.c
+++ src/encoding.c
@@ -6,42 +6,45 @@
 #include <string.h>
 
 char *encode(const char *s) {
-    char *r = NULL;
 
     /* Create a base64 filter. */
-    BIO *in = BIO_new(BIO_f_base64());
-    if (in == NULL)
-        goto fail;
-    BIO_set_flags(in, BIO_FLAGS_BASE64_NO_NL);
+    BIO *b64 = BIO_new(BIO_f_base64());
+    if (b64 == NULL)
+        return NULL;
+    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
 
     /* Create an in-memory sink to encode data into. */
     BIO *out = BIO_new(BIO_s_mem());
-    if (out == NULL)
-        goto fail;
+    if (out == NULL) {
+        BIO_free_all(b64);
+        return NULL;
+    }
 
-    BIO_push(in, out);
+    BIO *pipe = BIO_push(b64, out);
 
     int len = strlen(s);
 
     /* Encode the data. */
-    if (BIO_write(in, s, len) != len)
-        goto fail;
-    BIO_flush(in);
+    if (BIO_write(pipe, s, len) != len) {
+        BIO_free_all(pipe);
+        return NULL;
+    }
+    BIO_flush(pipe);
 
     /* Extract it into a string. */
     BUF_MEM *bptr;
     BIO_get_mem_ptr(out, &bptr);
 
-    r = malloc(bptr->length + 1);
-    if (r == NULL)
-        goto fail;
+    char *r = malloc(bptr->length + 1);
+    if (r == NULL) {
+        BIO_free_all(b64);
+        return NULL;
+    }
 
     memcpy(r, bptr->data, bptr->length);
     r[bptr->length] = '\0';
 
-fail:
-    if (in != NULL)
-        BIO_free_all(in);
+    BIO_free_all(b64);
 
     return r;
 }
