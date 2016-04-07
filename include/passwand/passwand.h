429001fe8aaf7810f68f487860796d2d6b4165d3
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-07 18:13:17 +1000

    Add basic data structure for an entry.

--- include/passwand/passwand.h
+++ include/passwand/passwand.h
@@ -1,5 +1,28 @@
 #pragma once
 
+#include <stdbool.h>
+
+typedef struct {
+
+    /* encrypted fields */
+    char *space;
+    char *key;
+    char *value;
+
+    /* hmac fields */
+    char *hmac;
+    char *hmac_salt;
+
+    /* other core fields */
+    char *salt;
+    char *iv;
+
+    /* fields that don't get exported */
+    bool encrypted;
+    unsigned int work_factor;
+
+} passwand_entry_t;
+
 /** Securely erase the memory backing a password.
  *
  * If input is the NULL pointer, this function is a no-op.
