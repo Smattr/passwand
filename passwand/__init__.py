32f8fc7f25d8ad3897ae57b0400659341e38c617
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-07 10:20:08 +1000

    Remove browser functionality.
    This relied on Chrome running in a debugging mode which is a fairly significant
    security risk.

--- passwand/__init__.py
+++ passwand/__init__.py
@@ -1,4 +1,3 @@
-from Browser import get_current_url
 from Dialogs import show_error, show_message, ask_password, ask_question
 from Encoding import decode, encode
 from Encryption import Encrypter, mac
