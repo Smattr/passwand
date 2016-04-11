56e3f147a468a7b908ffb2457729c7b66208d426
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-11 17:15:20 +1000

    Add some comments.

--- src/import.c
+++ src/import.c
@@ -11,45 +11,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#if 0
-char *read_file(const char *path) {
-    assert(path != NULL);
-
-    FILE *in __attribute__((cleanup(close))) = fopen(path, "r");
-    if (in == NULL)
-        return NULL;
-
-    char *buffer;
-    size_t buffer_len;
-    FILE *out = open_memstream(&buffer, &buffer_len);
-    if (out == NULL)
-        return NULL;
-
-    char buf[1024];
-    size_t read;
-    while ((read = fread(buf, 1, sizeof(buf), in)) != 0) {
-        do {
-            size_t written = fwrite(out, 1, read, out);
-            if (written == 0) {
-                fclose(out);
-                free(buffer);
-                return NULL;
-            }
-            assert(written <= read);
-            read -= written;
-        } while (read > 0);
-    }
-
-    fclose(out);
-
-    if (!feof(in)) {
-        free(buffer);
-        return NULL;
-    }
-
-    return buffer;
-}
-#endif
+/* The following are some helpers so we can use pervasive RAII in the import
+ * function.
+ */
 
 static void autoclose(void *p) {
     assert(p != NULL);
@@ -83,10 +47,12 @@ static void autojsonput(void *p) {
         json_object_put(*j);
 }
 
-int passwand_import(const char *path, passwand_entry_t **entries, unsigned *entry_len) {
+int passwand_import(const char *path, passwand_entry_t **entries,
+        unsigned *entry_len) {
     assert(entries != NULL);
     assert(entry_len != NULL);
 
+    /* MMap the input so JSON-C can stream it. */
     int f __attribute__((cleanup(autoclose))) = open(path, O_RDONLY);
     if (f == -1)
         return -1;
@@ -103,16 +69,21 @@ int passwand_import(const char *path, passwand_entry_t **entries, unsigned *entr
         .length = st.st_size,
     };
 
-    json_tokener *tok __attribute__((cleanup(autojsonfree))) = json_tokener_new();
+    /* Read the outer list. This should be the only item in the file. */
+    json_tokener *tok __attribute__((cleanup(autojsonfree)))
+        = json_tokener_new();
     if (tok == NULL)
         return -1;
 
-    json_object *j __attribute__((cleanup(autojsonput))) = json_tokener_parse_ex(tok, p, st.st_size);
+    json_object *j __attribute__((cleanup(autojsonput)))
+        = json_tokener_parse_ex(tok, p, st.st_size);
     if (j == NULL)
         return -1;
 
     if (!json_object_is_type(j, json_type_array))
         return -1;
+
+    /* We're now ready to start reading the entries themselves. */
 
     *entry_len = json_object_array_length(j);
     *entries = calloc(*entry_len, sizeof(passwand_entry_t));
@@ -124,6 +95,7 @@ int passwand_import(const char *path, passwand_entry_t **entries, unsigned *entr
         json_object *m = json_object_array_get_idx(j, i);
         assert(m != NULL);
         if (!json_object_is_type(m, json_type_object)) {
+            /* One of the array entries was not an object (dictionary). */
             free(*entries);
             return -1;
         }
@@ -132,11 +104,13 @@ int passwand_import(const char *path, passwand_entry_t **entries, unsigned *entr
     do { \
         json_object *v = json_object_object_get(m, #field); \
         if (v == NULL || !json_object_is_type(v, json_type_string)) { \
+            /* The value of this member was not a string. */ \
             free(*entries); \
             return -1; \
         } \
         (*entries)[i].field = decode(json_object_get_string(v)); \
         if ((*entries)[i].field == NULL) { \
+            /* The value was not valid base64 encoded. */ \
             free(*entries); \
             return -1; \
         } \
@@ -150,6 +124,9 @@ int passwand_import(const char *path, passwand_entry_t **entries, unsigned *entr
         GET(salt);
         GET(iv);
 
+        /* Mark all imported entries as encrypted as anything persistent on
+         * disk should have been encrypted during export.
+         */
         (*entries)[i].encrypted = true;
     }
 
