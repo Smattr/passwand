cf8b67498383c11d2a621dd22a1bdaa204f5631e
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-11 18:39:36 +1000

    Add some extra import tests.

--- tests/main.c
+++ tests/main.c
@@ -240,6 +240,97 @@ static void test_import_empty_list(void) {
     CU_ASSERT_EQUAL_FATAL(entry_len, 0);
 }
 
+static void test_import_missing_field(void) {
+    const char *data = "[{\"space\":\"aGVsbG8gd29ybGQ=\", \"key\":\"aGVsbG8gd29ybGQ=\", "
+        "\"value\":\"aGVsbG8gd29ybGQ=\", \"hmac\":\"aGVsbG8gd29ybGQ=\", \"hmac_salt\":"
+        "\"aGVsbG8gd29ybGQ=\", \"salt\":\"aGVsbG8gd29ybGQ=\"}]";
+
+    /* Create a temporary file. */
+    char tmp[sizeof("/tmp/tmp.XXXXXX")];
+    strcpy(tmp, "/tmp/tmp.XXXXXX");
+    int fd = mkstemp(tmp);
+    CU_ASSERT_NOT_EQUAL_FATAL(fd, -1);
+    ssize_t written = write(fd, data, strlen(data));
+    if (written != (ssize_t)strlen(data))
+        unlink(tmp);
+    CU_ASSERT_EQUAL_FATAL(written, strlen(data));
+
+    /* Now read in the entries */
+    passwand_entry_t *entries;
+    unsigned entry_len;
+    int r = passwand_import(tmp, &entries, &entry_len);
+    unlink(tmp);
+    CU_ASSERT_NOT_EQUAL_FATAL(r, 0);
+}
+
+static void test_import_basic(void) {
+    const char *data = "[{\"space\":\"aGVsbG8gd29ybGQ=\", \"key\":\"aGVsbG8gd29ybGQ=\", "
+        "\"value\":\"aGVsbG8gd29ybGQ=\", \"hmac\":\"aGVsbG8gd29ybGQ=\", \"hmac_salt\":"
+        "\"aGVsbG8gd29ybGQ=\", \"salt\":\"aGVsbG8gd29ybGQ=\", \"iv\":\"aGVsbG8gd29ybGQ=\"}]";
+
+    /* Create a temporary file. */
+    char tmp[sizeof("/tmp/tmp.XXXXXX")];
+    strcpy(tmp, "/tmp/tmp.XXXXXX");
+    int fd = mkstemp(tmp);
+    CU_ASSERT_NOT_EQUAL_FATAL(fd, -1);
+    ssize_t written = write(fd, data, strlen(data));
+    if (written != (ssize_t)strlen(data))
+        unlink(tmp);
+    CU_ASSERT_EQUAL_FATAL(written, strlen(data));
+
+    /* Now read in the entries */
+    passwand_entry_t *entries;
+    unsigned entry_len;
+    int r = passwand_import(tmp, &entries, &entry_len);
+    unlink(tmp);
+    CU_ASSERT_EQUAL_FATAL(r, 0);
+
+    /* Check we got an entry */
+    CU_ASSERT_EQUAL_FATAL(entry_len, 1);
+    CU_ASSERT_STRING_EQUAL(entries[0].space, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].key, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].value, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].hmac, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].hmac_salt, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].salt, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].iv, "hello world");
+}
+
+/* Test having a superfluous field is fine. */
+static void test_import_extra_field(void) {
+    const char *data = "[{\"space\":\"aGVsbG8gd29ybGQ=\", \"key\":\"aGVsbG8gd29ybGQ=\", "
+        "\"value\":\"aGVsbG8gd29ybGQ=\", \"hmac\":\"aGVsbG8gd29ybGQ=\", \"hmac_salt\":"
+        "\"aGVsbG8gd29ybGQ=\", \"salt\":\"aGVsbG8gd29ybGQ=\", \"iv\":\"aGVsbG8gd29ybGQ=\","
+        "\"extra\":\"blah blah\"}]";
+
+    /* Create a temporary file. */
+    char tmp[sizeof("/tmp/tmp.XXXXXX")];
+    strcpy(tmp, "/tmp/tmp.XXXXXX");
+    int fd = mkstemp(tmp);
+    CU_ASSERT_NOT_EQUAL_FATAL(fd, -1);
+    ssize_t written = write(fd, data, strlen(data));
+    if (written != (ssize_t)strlen(data))
+        unlink(tmp);
+    CU_ASSERT_EQUAL_FATAL(written, strlen(data));
+
+    /* Now read in the entries */
+    passwand_entry_t *entries;
+    unsigned entry_len;
+    int r = passwand_import(tmp, &entries, &entry_len);
+    unlink(tmp);
+    CU_ASSERT_EQUAL_FATAL(r, 0);
+
+    /* Check we got an entry */
+    CU_ASSERT_EQUAL_FATAL(entry_len, 1);
+    CU_ASSERT_STRING_EQUAL(entries[0].space, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].key, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].value, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].hmac, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].hmac_salt, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].salt, "hello world");
+    CU_ASSERT_STRING_EQUAL(entries[0].iv, "hello world");
+}
+
 static const struct {
     const char *name;
     void (*fn)(void);
@@ -258,6 +349,9 @@ static const struct {
     TEST(test_export_basic),
     TEST(test_export_unencrypted),
     TEST(test_import_empty_list),
+    TEST(test_import_basic),
+    TEST(test_import_missing_field),
+    TEST(test_import_extra_field),
 #undef TEST
 };
 
