3498cce374b5f3f63c66a450dcc5d4dffed52121
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-11 19:54:20 +1000

    Change calling convention of make_key.

--- src/encryption.c
+++ src/encryption.c
@@ -10,27 +10,21 @@
 
 static const size_t KEY_SIZE = 32; // bytes
 
-uint8_t *make_key(const uint8_t *master, size_t master_len, const uint8_t *salt,
-        size_t salt_len, int work_factor) {
+int make_key(const uint8_t *master, size_t master_len, const uint8_t *salt,
+        size_t salt_len, int work_factor, uint8_t *buffer) {
 
     if (work_factor == -1)
         work_factor = 14; // default value
 
     if (work_factor < 10 || work_factor > 31)
-        return NULL;
+        return -1;
 
     static const uint32_t r = 8;
     static const uint32_t p = 1;
 
-    uint8_t *buffer = malloc(KEY_SIZE);
-    if (buffer == NULL)
-        return NULL;
-
     if (libscrypt_scrypt(master, master_len, salt, salt_len,
-            ((uint64_t)1) << work_factor, r, p, buffer, KEY_SIZE) != 0) {
-        free(buffer);
-        return NULL;
-    }
+            ((uint64_t)1) << work_factor, r, p, buffer, KEY_SIZE) != 0)
+        return -1;
 
-    return buffer;
+    return 0;
 }
