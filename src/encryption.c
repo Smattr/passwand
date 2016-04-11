a40d8c38dbe80994e6cb6635ae1e253c243e144e
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-11 20:00:28 +1000

    Implement random_bytes.

--- src/encryption.c
+++ src/encryption.c
@@ -4,11 +4,36 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <fcntl.h>
 #include <libscrypt.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static const size_t KEY_SIZE = 32; // bytes
+
+int random_bytes(uint8_t *buffer, size_t buffer_len) {
+    /* XXX: This should really use getrandom when it's more widely available. */
+
+    /* This is the limit at which getrandom can fail. For this library we
+     * shouldn't ever need to read more than 16 bytes.
+     */
+    if (buffer_len > 256)
+        return -1;
+
+    int fd = open("/dev/random", O_RDONLY);
+    if (fd == -1)
+        return -1;
+
+    ssize_t r = read(fd, buffer, buffer_len);
+    close(fd);
+    if (r != (ssize_t)buffer_len)
+        return -1;
+
+    return 0;
+}
 
 int make_key(const uint8_t *master, size_t master_len, const uint8_t *salt,
         size_t salt_len, int work_factor, uint8_t *buffer) {
