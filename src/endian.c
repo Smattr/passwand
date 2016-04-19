67b41dee895abc1c6c8521c75b281d712bfeecc2
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-19 15:36:37 +1000

    Another crack at featuring endianness.
    I'm a bit rusty on this stuff and don't have any big endian hardware available
    so ugh.

--- src/endian.c
+++ src/endian.c
@@ -6,11 +6,11 @@
 unsigned __int128 htole128(unsigned __int128 host_128bits) {
     unsigned __int128 low = htole64(host_128bits);
     unsigned __int128 high = htole64(host_128bits >> 64);
-    return (high << 64) | low;
+    return (low << 64) | high;
 }
 
 unsigned __int128 le128toh(unsigned __int128 little_endian_128bits) {
     unsigned __int128 low = le64toh(little_endian_128bits);
     unsigned __int128 high = le64toh(little_endian_128bits >> 64);
-    return (high << 64) | low;
+    return (low << 64) | high;
 }
