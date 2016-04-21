e39c3e0f55d39da5fb1228816380a8dfd9c88c4a
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-21 16:31:20 +1000

    Extend entry_new test.

--- tests/test_entry_new.c
+++ tests/test_entry_new.c
@@ -4,13 +4,18 @@
 #include "test.h"
 
 TEST("entry_new: basic functionality") {
-    passwand_entry_t e;
+    passwand_entry_t e = { NULL };
     char *master = "hello world";
     char *space = "space";
     char *key = "key";
     char *value = "value";
     passwand_error_t err = passwand_entry_new(&e, master, space, key, value, 14);
     CU_ASSERT_EQUAL_FATAL(err, PW_OK);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(e.space);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(e.key);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(e.value);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(e.hmac);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(e.hmac_salt);
     free(e.space);
     free(e.key);
     free(e.value);
