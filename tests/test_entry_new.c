09782d78699fc415fec7201192b4c70f8e7c7a18
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-21 16:57:13 +1000

    Add some more tests of entry_new.

--- tests/test_entry_new.c
+++ tests/test_entry_new.c
@@ -1,14 +1,17 @@
 #include <CUnit/CUnit.h>
 #include <passwand/passwand.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include "test.h"
+
+static const char *master = "hello world";
+static const char *space = "space";
+static const char *key = "key";
+static const char *value = "value";
 
 TEST("entry_new: basic functionality") {
     passwand_entry_t e = { NULL };
-    char *master = "hello world";
-    char *space = "space";
-    char *key = "key";
-    char *value = "value";
     passwand_error_t err = passwand_entry_new(&e, master, space, key, value, 14);
     CU_ASSERT_EQUAL_FATAL(err, PW_OK);
     CU_ASSERT_PTR_NOT_NULL_FATAL(e.space);
@@ -16,6 +19,43 @@ TEST("entry_new: basic functionality") {
     CU_ASSERT_PTR_NOT_NULL_FATAL(e.value);
     CU_ASSERT_PTR_NOT_NULL_FATAL(e.hmac);
     CU_ASSERT_PTR_NOT_NULL_FATAL(e.hmac_salt);
+    free(e.space);
+    free(e.key);
+    free(e.value);
+    free(e.hmac);
+    free(e.hmac_salt);
+}
+
+TEST("entry_new: check_mac(entry_new(...))") {
+    passwand_entry_t e;
+    passwand_error_t err = passwand_entry_new(&e, master, space, key, value, 14);
+    CU_ASSERT_EQUAL_FATAL(err, PW_OK);
+
+    err = passwand_entry_check_mac(master, &e);
+    CU_ASSERT_EQUAL_FATAL(err, PW_OK);
+
+    free(e.space);
+    free(e.key);
+    free(e.value);
+    free(e.hmac);
+    free(e.hmac_salt);
+}
+
+TEST("entry_new: recoverable") {
+    passwand_entry_t e;
+    passwand_error_t err = passwand_entry_new(&e, master, space, key, value, 14);
+    CU_ASSERT_EQUAL_FATAL(err, PW_OK);
+
+    void check(void *state, const char *s, const char *k, const char *v) {
+        bool *st = state;
+        *st = strcmp(space, s) == 0 && strcmp(key, k) == 0 && strcmp(value, v) == 0;
+    }
+
+    bool checked = false;
+    err = passwand_entry_do(master, &e, check, &checked);
+    CU_ASSERT_EQUAL_FATAL(err, PW_OK);
+    CU_ASSERT_EQUAL_FATAL(checked, true);
+
     free(e.space);
     free(e.key);
     free(e.value);
