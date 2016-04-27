2998bbc199d9a3d835163ead528b53b807446fad
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-04-27 22:32:10 +1000

    Extra endian functions.

--- src/endian.c
+++ src/endian.c
@@ -15,6 +15,16 @@ uint128_t le128toh(uint128_t little_endian_128bits) {
     return little_endian_128bits;
 }
 
+uint128_t htobe128(uint128_t host_128bits) {
+    return (((uint128_t)htobe64(host_128bits)) << 64) |
+            ((uint128_t)htobe64(host_128bits >> 64));
+}
+
+uint128_t be128toh(uint128_t big_endian_128bits) {
+    return (((uint128_t)be64toh(big_endian_128bits)) << 64) |
+            ((uint128_t)be64toh(big_endian_128bits >> 64));
+}
+
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 
 uint128_t htole128(uint128_t host_128bits) {
@@ -26,4 +36,13 @@ uint128_t le128toh(uint128_t little_endian_128bits) {
     return (((uint128_t)le64toh(little_endian_128bits)) << 64) |
             ((uint128_t)le64toh(little_endian_128bits >> 64));
 }
+
+uint128_t htobe128(uint128_t host_128bits) {
+    return host_128bits;
+}
+
+uint128_t be128toh(uint128_t big_endian_128bits) {
+    return big_endian_128bits;
+}
+
 #endif
--- src/endian.h
+++ src/endian.h
@@ -5,3 +5,7 @@
 unsigned __int128 htole128(unsigned __int128 host_128bits);
 
 unsigned __int128 le128toh(unsigned __int128 little_endian_128bits);
+
+unsigned __int128 htobe128(unsigned __int128 host_128bits);
+
+unsigned __int128 be128toh(unsigned __int128 big_endian_128bits);
