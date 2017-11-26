2e20a8b092f2338c7f86f9ec8eeea3e5e2bcb948
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-11-25 17:38:39 -0800

    tests: squash a -Wmissing-field-initializers warning
    Related to Github #1 'Clang compatibility'.

--- tests/test_entry_new.c
+++ tests/test_entry_new.c
@@ -11,7 +11,8 @@ static const char *key = "key";
 static const char *value = "value";
 
 TEST("entry_new: basic functionality") {
-    passwand_entry_t e = { NULL };
+    passwand_entry_t e;
+    memset(&e, 0, sizeof(e));
     passwand_error_t err = passwand_entry_new(&e, master, space, key, value, 14);
     CU_ASSERT_EQUAL_FATAL(err, PW_OK);
     CU_ASSERT_PTR_NOT_NULL_FATAL(e.space);
