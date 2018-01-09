edcd72a4c6c3bfa94ab030e4e57a02ebc015e5aa
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-01-08 18:15:31 -0800

    add basic setup for writing integration tests

---
+++ tests/integration-tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+'''
+Framework for writing integration tests.
+'''
+
+import unittest
+
+class DummyTest(unittest.TestCase):
+    def test_dummy(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
