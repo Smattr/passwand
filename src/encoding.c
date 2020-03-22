20d370f38cd6d9c91ded2fb73cb1eaafa49e059d
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-22 09:20:32 -0700

    rewrite base 64 encoding in goto-style
    The former cleanup-style apparently did not prevent us introducing memory leaks
    (see commit 403b8809b44e848bacddb07c0331520a2352815b), so we may as well revert
    to something more standardised and widely supported.

--- src/encoding.c
+++ src/encoding.c
@@ -8,13 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void autobiofree(void *p) {
-    assert(p != NULL);
-    BIO **b = p;
-    if (*b != NULL)
-        BIO_free_all(*b);
-}
-
 passwand_error_t encode(const uint8_t *s, size_t len, char **e) {
 
     assert(s != NULL);
@@ -24,37 +17,49 @@ passwand_error_t encode(const uint8_t *s, size_t len, char **e) {
         return PW_OVERFLOW;
 
     /* Create a base64 filter. */
-    BIO *b64 __attribute__((cleanup(autobiofree))) = BIO_new(BIO_f_base64());
+    BIO *b64 = BIO_new(BIO_f_base64());
     if (b64 == NULL)
         return PW_NO_MEM;
     BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
 
+    passwand_error_t rc = PW_OK;
+
     /* Create an in-memory sink to encode data into. */
     BIO *out = BIO_new(BIO_s_mem());
-    if (out == NULL)
-        return PW_NO_MEM;
+    if (out == NULL) {
+        rc = PW_NO_MEM;
+        goto done;
+    }
 
     b64 = BIO_push(b64, out);
 
     /* Encode the data. */
-    if (BIO_write(b64, s, len) != (int)len)
-        return PW_IO;
+    if (BIO_write(b64, s, len) != (int)len) {
+        rc = PW_IO;
+        goto done;
+    }
     BIO_flush(b64);
 
     /* Extract it into a string. */
     BUF_MEM *bptr;
     BIO_get_mem_ptr(out, &bptr);
 
-    if (SIZE_MAX - 1 < bptr->length)
-        return PW_OVERFLOW;
+    if (SIZE_MAX - 1 < bptr->length) {
+        rc = PW_OVERFLOW;
+        goto done;
+    }
     *e = malloc(bptr->length + 1);
-    if (*e == NULL)
-        return PW_NO_MEM;
+    if (*e == NULL) {
+        rc = PW_NO_MEM;
+        goto done;
+    }
 
     memcpy(*e, bptr->data, bptr->length);
     (*e)[bptr->length] = '\0';
 
-    return PW_OK;
+done:
+    BIO_free_all(b64);
+    return rc;
 }
 
 passwand_error_t decode(const char *s, uint8_t **d, size_t *len) {
@@ -62,17 +67,22 @@ passwand_error_t decode(const char *s, uint8_t **d, size_t *len) {
     assert(s != NULL);
     assert(d != NULL);
 
+    *d = NULL;
+
     /* Create a base64 filter. */
-    BIO *b64 __attribute__((cleanup(autobiofree))) = BIO_new(BIO_f_base64());
+    BIO *b64 = BIO_new(BIO_f_base64());
     if (b64 == NULL)
         return PW_NO_MEM;
     BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
 
+    passwand_error_t rc = PW_OK;
 
     /* Create a source to read encoded data from. */
     BIO *in = BIO_new_mem_buf((void*)s, -1);
-    if (in == NULL)
-        return PW_NO_MEM;
+    if (in == NULL) {
+        rc = PW_NO_MEM;
+        goto done;
+    }
 
     b64 = BIO_push(b64, in);
 
@@ -82,19 +92,26 @@ passwand_error_t decode(const char *s, uint8_t **d, size_t *len) {
     BUF_MEM *pp __attribute__((unused));
     long sz = BIO_get_mem_data(b64, &pp);
     *d = malloc(sz);
-    if (*d == NULL)
-        return PW_NO_MEM;
+    if (*d == NULL) {
+        rc = PW_NO_MEM;
+        goto done;
+    }
 
     /* Do the actual decoding. */
     int read = BIO_read(b64, *d, sz);
 
     assert((long)read <= sz);
     if (read < 0) {
-        free(*d);
-        *d = NULL;
-        return PW_IO;
+        rc = PW_IO;
+        goto done;
     }
     *len = read;
 
-    return PW_OK;
+done:
+    if (rc != PW_OK) {
+        free(*d);
+        *d = NULL;
+    }
+    BIO_free_all(b64);
+    return rc;
 }
