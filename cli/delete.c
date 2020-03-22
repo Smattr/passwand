353561bd63c7451a47685f122c3bad5011fd6e06
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-22 08:44:48 -0700

    cli: fix double free when deleting entries
    After introducing the free on exit code (commit
    4e307e3d441fff9e35a6147876d1c051c7660d89) we were then freeing an entry twice;
    the last one that gets shuffled back one position during a delete and thus
    occupies both the last and second-to-last slot. We were also leaking memory
    related to the deleted entry. To fix both these issues, we replace the entry in
    the last slot with the deleted one.

--- cli/delete.c
+++ cli/delete.c
@@ -59,8 +59,14 @@ static int finalize(void) {
     }
 
     /* Shuffle entries following the one to be deleted, to remove the deleted one. */
+    passwand_entry_t deleted = saved_entries[found_index];
     for (size_t i = found_index; i < saved_entry_len - 1; i++)
         saved_entries[i] = saved_entries[i + 1];
+
+    /* Put the deleted entry in the last place in the list, unused by us, but will be freed by
+     * main().
+     */
+    saved_entries[saved_entry_len - 1] = deleted;
 
     passwand_error_t err = passwand_export(options.data, saved_entries, saved_entry_len - 1);
     if (err != PW_OK) {
