cbe13e84feedbfbbeab629a3253c85a847dd3715
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-03-09 20:12:31 -0800

    gtk: move X11 functionality to its own file
    This prepares us for implementing the equivalent for Wayland in a separate file.

--- gui/gtk.c
+++ gui/gtk.c
@@ -1,9 +1,7 @@
-// implementation of the API described in gui.h using GTK 2/3
+// implementation of part of the API described in gui.h using GTK 2/3
 
-#include "../common/getenv.h"
 #include "gtk_lock.h"
 #include "gui.h"
-#include <X11/Xlib.h>
 #include <assert.h>
 #include <gtk/gtk.h>
 #include <passwand/passwand.h>
@@ -95,30 +93,6 @@ char *get_text(const char *title, const char *message, const char *initial,
   return r;
 }
 
-static void send_char(Display *display, Window window, char c) {
-
-  assert(supported_upper(c) || supported_lower(c));
-
-  XKeyEvent e = {
-      .display = display,
-      .window = window,
-      .root = RootWindow(display, DefaultScreen(display)),
-      .subwindow = None,
-      .time = CurrentTime,
-      .x = 1,
-      .y = 1,
-      .x_root = 1,
-      .y_root = 1,
-      .same_screen = True,
-      .type = KeyPress,
-      .state = supported_upper(c) ? ShiftMask : 0,
-      .keycode = XKeysymToKeycode(display, c),
-  };
-  XSendEvent(display, window, True, KeyPressMask, (XEvent *)&e);
-  e.type = KeyRelease;
-  XSendEvent(display, window, True, KeyReleaseMask, (XEvent *)&e);
-}
-
 void flush_state(void) {
 
   int err __attribute__((unused)) = pthread_mutex_lock(&gtk_lock);
@@ -130,50 +104,6 @@ void flush_state(void) {
 
   err = pthread_mutex_unlock(&gtk_lock);
   assert(err == 0);
-}
-
-int send_text(const char *text) {
-
-  assert(text != NULL);
-
-  int err __attribute__((unused)) = pthread_mutex_lock(&gtk_lock);
-  assert(err == 0);
-
-  // find the current display
-  const char *display = getenv_("DISPLAY");
-  if (display == NULL)
-    display = ":0";
-  Display *d = XOpenDisplay(display);
-  if (d == NULL) {
-    err = pthread_mutex_unlock(&gtk_lock);
-    assert(err == 0);
-    show_error("failed to open X11 display");
-    return -1;
-  }
-
-  // find the active window
-  Window win;
-  int state;
-  XGetInputFocus(d, &win, &state);
-  if (win == None) {
-    XCloseDisplay(d);
-    err = pthread_mutex_unlock(&gtk_lock);
-    assert(err == 0);
-    show_error("no window focused");
-    return -1;
-  }
-
-  for (size_t i = 0; i < strlen(text); i++) {
-    assert(supported_upper(text[i]) || supported_lower(text[i]));
-    send_char(d, win, text[i]);
-  }
-
-  XCloseDisplay(d);
-
-  err = pthread_mutex_unlock(&gtk_lock);
-  assert(err == 0);
-
-  return 0;
 }
 
 void show_error(const char *message) {
