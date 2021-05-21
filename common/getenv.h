b5c8f7c59bfbb857704a096b972888b76aa44168
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-05-20 18:34:43 -0700

    avoid a -Wundef warning when building with non-Gibc libc

--- common/getenv.h
+++ common/getenv.h
@@ -4,9 +4,10 @@
 
 // wrapper around getenv to call secure_getenv when available
 static inline char *getenv_(const char *name) {
+#ifdef __GLIBC__
 #if __GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 17)
   return secure_getenv(name);
-#else
-  return getenv(name);
 #endif
+#endif
+  return getenv(name);
 }
