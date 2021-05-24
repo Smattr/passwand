d1aeb1f10be8eb899212c2ecea227e3fde53c3c5
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-05-23 17:23:02 -0700

    expose functions for the `set` command beyond set.c
    We will want to reuse these for the upcoming `generate` command.
    
    Github: related to #21 "generate command"

--- cli/set.c
+++ cli/set.c
@@ -17,8 +17,8 @@ static size_t saved_entry_len;
 static atomic_bool found;
 static _Thread_local size_t candidate_index;
 
-static int initialize(const main_t *mainpass, passwand_entry_t *entries,
-                      size_t entry_len) {
+int set_initialize(const main_t *mainpass, passwand_entry_t *entries,
+                   size_t entry_len) {
 
   saved_main = mainpass;
   saved_entries = entries;
@@ -40,12 +40,12 @@ static int initialize(const main_t *mainpass, passwand_entry_t *entries,
   return 0;
 }
 
-static void loop_notify(size_t entry_index) { candidate_index = entry_index; }
+void set_loop_notify(size_t entry_index) { candidate_index = entry_index; }
 
-static bool loop_condition(void) { return !found; }
+bool set_loop_condition(void) { return !found; }
 
-static void loop_body(const char *space, const char *key,
-                      const char *value __attribute__((unused))) {
+void set_loop_body(const char *space, const char *key,
+                   const char *value __attribute__((unused))) {
 
   assert(space != NULL);
   assert(key != NULL);
@@ -60,7 +60,7 @@ static void loop_body(const char *space, const char *key,
   }
 }
 
-static int finalize(void) {
+int set_finalize(void) {
 
   if (found) {
     eprint("an entry for %s/%s already exists\n", options.space, options.key);
@@ -113,9 +113,9 @@ const command_t set = {
     .need_value = REQUIRED,
     .need_length = DISALLOWED,
     .access = LOCK_EX,
-    .initialize = initialize,
-    .loop_notify = loop_notify,
-    .loop_condition = loop_condition,
-    .loop_body = loop_body,
-    .finalize = finalize,
+    .initialize = set_initialize,
+    .loop_notify = set_loop_notify,
+    .loop_condition = set_loop_condition,
+    .loop_body = set_loop_body,
+    .finalize = set_finalize,
 };
--- cli/set.h
+++ cli/set.h
@@ -1,5 +1,19 @@
 #pragma once
 
 #include "cli.h"
+#include <stdbool.h>
+#include <stddef.h>
 
 extern const command_t set;
+
+int set_initialize(const main_t *mainpass, passwand_entry_t *entries,
+                   size_t entry_len) __attribute__((visibility("hidden")));
+
+void set_loop_notify(size_t entry_index) __attribute__((visibility("hidden")));
+
+bool set_loop_condition(void) __attribute__((visibility("hidden")));
+
+void set_loop_body(const char *space, const char *key,
+                   const char *value) __attribute__((visibility("hidden")));
+
+int set_finalize(void) __attribute__((visibility("hidden")));
