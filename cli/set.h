4f7ef1055c53c11cae448cc9cc76ca6d53e11e31
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-05-25 07:43:08 -0700

    some clang-format tweaks

--- cli/set.h
+++ cli/set.h
@@ -13,7 +13,7 @@ void set_loop_notify(size_t entry_index) __attribute__((visibility("hidden")));
 
 bool set_loop_condition(void) __attribute__((visibility("hidden")));
 
-void set_loop_body(const char *space, const char *key,
-                   const char *value) __attribute__((visibility("hidden")));
+void set_loop_body(const char *space, const char *key, const char *value)
+    __attribute__((visibility("hidden")));
 
 int set_finalize(void) __attribute__((visibility("hidden")));
